54ba7e814469acc1a99d9fc82fd2cc2e0c7513e9
Python_John <pythonjohn@users.noreply.github.com>
2016-01-18 10:09:34 -0500

    Create Python_Johns Homework 1_24_2016.py

---
+++ Python_Johns Homework 1_24_2016.py
@@ -0,0 +1,12 @@
+# Weekly Project: ROCK PAPER SCISSORS
+# I will be using Python 3 coding in this game.
+#
+#
+print ('This is Python_Johns Homework for sliceofpython reddit group')
+print (' Its [Week 2] Sections 4-7 from Automate the Boring Stuff with Python.')
+print ('Weekly Project: ROCK PAPER SCISSORS')
+print()
+print()
+print ('User picks rock, paper, scissors. Computer picks. Determine the winner.')
+print()
+#
